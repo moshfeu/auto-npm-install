03d3aada09bcf1736701f3290c7f873db1dd82ca
Mosh Feu <moshfeu@gmail.com>
2018-09-23 14:25:20 +0300

    detect package is installed

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,23 +10,26 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "auto-npm-install" is now active!');
 
   vscode.window.onDidChangeTextEditorSelection(e => {
+    const activeEditor = vscode.window.activeTextEditor;
     const { text } = activeEditor.document.lineAt(e.selections[0].start.line);
+    const pack = getPackageFromImport(text);
 
-    if (isImportLine(text)) {
-      console.log('import line');
+    if (pack) {
+      const isPackageInstalled = fs.existsSync(`${vscode.workspace.rootPath}/node_modules/${pack}`);
+      console.log('isPackageInstalled', isPackageInstalled);
     } else {
       console.log('not import line');
     }
   });
-
-  const activeEditor = vscode.window.activeTextEditor;
-  if (activeEditor) {
-    console.log(activeEditor.selection.active.line);
-  }
 }
 
-const isImportLine = (text: string) => {
-  return /^import (.*) from (.*);/.exec(text);
+const getPackageFromImport = (text: string): string => {
+  try {
+    const [,,packageName] = /^import (.*) from ['|"](.*)['|"];/.exec(text) || ['', '', ''];
+    return packageName;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // this method is called when your extension is deactivated
